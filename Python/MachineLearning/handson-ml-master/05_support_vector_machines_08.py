2100e46a93b1a4ea598910d7a723f45341c9983d
和視 宗片 <kazumikm0119@pi5.fiberbit.net>
2019-10-21 01:33:32 +0900

    Extra materialのTraining timeの一部を追加する。

---
+++ Python/MachineLearning/handson-ml-master/05_support_vector_machines_08.py
@@ -0,0 +1,110 @@
+# common library
+import time
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn import datasets
+from sklearn.base import BaseEstimator
+from sklearn.datasets import make_moons
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import PolynomialFeatures
+from sklearn.preprocessing import StandardScaler
+from mpl_toolkits.mplot3d import Axes3D
+from sklearn.svm import LinearSVC
+from sklearn.svm import LinearSVR
+from sklearn.svm import SVC
+from sklearn.svm import SVR
+
+print('------------------------------------------------------------------------------------------------------\n'
+      '                           Extra material                                                             \n'
+      '------------------------------------------------------------------------------------------------------\n')
+X, y = make_moons(n_samples=1000, noise=0.4, random_state=42)
+plt.plot(X[:, 0][y==0], X[:, 1][y==0], "bs")
+plt.plot(X[:, 0][y==1], X[:, 1][y==1], "g^")
+plt.show()
+
+tol = 0.1
+tols = []
+times = []
+
+for i in range(10):
+    svm_clf = SVC(kernel='poly', gamma=3, C=10, tol=tol, verbose=1)
+    t1 = time.time()
+    svm_clf.fit(X, y)
+    t2 = time.time()
+    times.append(t2 - t1)
+    tols.append(tol)
+    print(i, tol, t2 - t1)
+    tol /= 10
+
+plt.semilogx(tols, times)
+plt.show()
+
+print('------------------------------------------------------------------------------------------------------\n'
+      '          Linear SVM classifier implementation using Batch Gradient Descent                           \n'
+      '------------------------------------------------------------------------------------------------------\n')
+# Get iris from the dataset.
+iris = datasets.load_iris()
+
+# Training set
+X = iris["data"][:, (2, 3)] # petal length, petal width
+y = (iris["target"] == 2).astype(np.float64).reshape(-1, 1) # Iris-Virginica
+
+class MyLinearSVC(BaseEstimator):
+    def __init__(self, C=1, eta0=1, eta_d=10000, n_epochs=1000, random_state=None):
+        self.C = C
+        self.eta0 = eta0
+        self.n_epochs = n_epochs
+        self.random_state = random_state
+        self.eta_d = eta_d
+
+    def eta(self, epoch):
+        return self.eta0 / (epoch + self.eta_d)
+        
+    def fit(self, X, y):
+        # Random initialization
+        if self.random_state:
+            np.random.seed(self.random_state)
+        w = np.random.randn(X.shape[1], 1) # n feature weights
+        b = 0
+
+        m = len(X)
+        t = y * 2 - 1  # -1 if t==0, +1 if t==1
+        X_t = X * t
+        self.Js=[]
+
+        # Training
+        for epoch in range(self.n_epochs):
+            support_vectors_idx = (X_t.dot(w) + t * b < 1).ravel()
+            X_t_sv = X_t[support_vectors_idx]
+            t_sv = t[support_vectors_idx]
+
+            J = 1/2 * np.sum(w * w) + self.C * (np.sum(1 - X_t_sv.dot(w)) - b * np.sum(t_sv))
+            self.Js.append(J)
+
+            w_gradient_vector = w - self.C * np.sum(X_t_sv, axis=0).reshape(-1, 1)
+            b_derivative = -C * np.sum(t_sv)
+                
+            w = w - self.eta(epoch) * w_gradient_vector
+            b = b - self.eta(epoch) * b_derivative
+            
+
+        self.intercept_ = np.array([b])
+        self.coef_ = np.array([w])
+        support_vectors_idx = (X_t.dot(w) + t * b < 1).ravel()
+        self.support_vectors_ = X[support_vectors_idx]
+        return self
+
+    def decision_function(self, X):
+        return X.dot(self.coef_[0]) + self.intercept_[0]
+
+    def predict(self, X):
+        return (self.decision_function(X) >= 0).astype(np.float64)
+
+C=2
+svm_clf = MyLinearSVC(C=C, eta0 = 10, eta_d = 1000, n_epochs=60000, random_state=2)
+svm_clf.fit(X, y)
+svm_clf_predict = svm_clf.predict(np.array([[5, 2], [4, 1]]))
+print('svm_clf_predict = \n{0}'.format(svm_clf_predict))
+print()
+
+
