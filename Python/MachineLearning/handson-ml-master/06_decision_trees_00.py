20abc2d13d533d0c89b92906ef5ac26e134fff5f
munezou <kazumikm0119@pi5.fiberbit.net>
2019-11-10 02:59:21 +0900

    handson-ml-master/06_decision_tree_00.pyにsource codeを追加する。

---
+++ Python/MachineLearning/handson-ml-master/06_decision_trees_00.py
@@ -0,0 +1,44 @@
+# Common imports
+import numpy as np
+import os
+from sklearn.datasets import load_iris
+from sklearn.tree import DecisionTreeClassifier
+
+# to make this notebook's output stable across runs
+np.random.seed(42)
+
+# To plot pretty figures
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+mpl.rc('axes', labelsize=14)
+mpl.rc('xtick', labelsize=12)
+mpl.rc('ytick', labelsize=12)
+
+# Where to save the figures
+PROJECT_ROOT_DIR = os.path.join(os.path.abspath(os.path.dirname(__file__)))
+CHAPTER_ID = "decision_trees"
+
+def image_path(fig_id):
+    return os.path.join(PROJECT_ROOT_DIR, "images", CHAPTER_ID, fig_id)
+
+def save_fig(fig_id, tight_layout=True):
+    print("Saving figure", fig_id)
+    if tight_layout:
+        plt.tight_layout()
+    plt.savefig(image_path(fig_id) + ".png", format='png', dpi=300)
+
+'''
+-----------------------------------------------------------------------------------
+6.1 Decision tree training and visualization
+-----------------------------------------------------------------------------------
+'''
+iris = load_iris()
+
+print('iris information = \n{0}\n'.format(iris.DESCR))
+
+X = iris.data[:, 2:]    # petal length and width
+y = iris.target
+
+tree_clf = DecisionTreeClassifier(max_depth=2, random_state=42)
+tree_clf_fit = tree_clf.fit(X, y)
+print('tree_clf_fit = \n{0}\n'.format(tree_clf_fit))
