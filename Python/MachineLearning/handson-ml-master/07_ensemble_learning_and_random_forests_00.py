cd7c1482c3a2ed0f677b0b4c0bdc623d9001c1ab
munezou <kazumikm0119@pi5.fiberbit.net>
2019-11-15 01:14:16 +0900

    handson_ml_master/07_ensemble_learning_and_random_forests_00.pyにsource codeを追加する。

--- Python/MachineLearning/handson-ml-master/07_ensemble_learning_and_random_forests_00.py
+++ Python/MachineLearning/handson-ml-master/07_ensemble_learning_and_random_forests_00.py
@@ -7,9 +7,12 @@ from sklearn.model_selection import train_test_split
 from sklearn.datasets import make_moons
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.ensemble import VotingClassifier
+from sklearn.ensemble import BaggingClassifier
 from sklearn.linear_model import LogisticRegression
 from sklearn.svm import SVC
 from sklearn.metrics import accuracy_score
+from sklearn.tree import DecisionTreeClassifier
+from matplotlib.colors import ListedColormap
 
 '''
 --------------------------------------------------------------------
@@ -127,3 +130,59 @@ for clf in (log_clf, rnd_clf, svm_clf, voting_clf):
 
 print()
 
+print('------------------------------------------------------------------------------------------------------\n'
+      '          7.2.1 Bagging and pasting in scikit-learn                                                   \n'
+      '------------------------------------------------------------------------------------------------------\n')
+print('---< prepare a Bagging Classifier >---')
+bag_clf = BaggingClassifier(
+    DecisionTreeClassifier(random_state=42), n_estimators=500,
+    max_samples=100, bootstrap=True, n_jobs=-1, random_state=42)
+
+# fitting
+bag_clf_fit = bag_clf.fit(X_train, y_train)
+print('bag_clf_fit = \n{0}\n'.format(bag_clf_fit))
+
+# accuracy result
+y_pred = bag_clf.predict(X_test)
+
+# compare target and predict
+print('accuracy_score(y_test, y_pred) = {0}\n'.format(accuracy_score(y_test, y_pred)))
+
+print('---< prepare a Decision trees >---')
+tree_clf = DecisionTreeClassifier(random_state=42)
+
+# fitting
+tree_clf.fit(X_train, y_train)
+
+# accuracy result
+y_pred_tree = tree_clf.predict(X_test)
+
+# compare target and predict
+print('accuracy_score(y_test, y_pred_tree) = {0}\n'.format(accuracy_score(y_test, y_pred_tree)))
+
+def plot_decision_boundary(clf, X, y, axes=[-1.5, 2.5, -1, 1.5], alpha=0.5, contour=True):
+    x1s = np.linspace(axes[0], axes[1], 100)
+    x2s = np.linspace(axes[2], axes[3], 100)
+    x1, x2 = np.meshgrid(x1s, x2s)
+    X_new = np.c_[x1.ravel(), x2.ravel()]
+    y_pred = clf.predict(X_new).reshape(x1.shape)
+    custom_cmap = ListedColormap(['#fafab0','#9898ff','#a0faa0'])
+    plt.contourf(x1, x2, y_pred, alpha=0.3, cmap=custom_cmap)
+    if contour:
+        custom_cmap2 = ListedColormap(['#7d7d58','#4c4c7f','#507d50'])
+        plt.contour(x1, x2, y_pred, cmap=custom_cmap2, alpha=0.8)
+    plt.plot(X[:, 0][y==0], X[:, 1][y==0], "yo", alpha=alpha)
+    plt.plot(X[:, 0][y==1], X[:, 1][y==1], "bs", alpha=alpha)
+    plt.axis(axes)
+    plt.xlabel(r"$x_1$", fontsize=18)
+    plt.ylabel(r"$x_2$", fontsize=18, rotation=0)
+
+plt.figure(figsize=(11,4))
+plt.subplot(121)
+plot_decision_boundary(tree_clf, X, y)
+plt.title("Decision Tree", fontsize=14)
+plt.subplot(122)
+plot_decision_boundary(bag_clf, X, y)
+plt.title("Decision Trees with Bagging", fontsize=14)
+save_fig("decision_tree_without_and_with_bagging_plot")
+plt.show()
