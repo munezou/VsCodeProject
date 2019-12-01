3cd73a93a632817934bc4f074530e37a467d460d
munezou <kazumikm0119@pi5.fiberbit.net>
2019-12-01 21:07:55 +0900

    handson-ml-master/08_dimensionality_reduction_02.pyにsource codeを追加する。

--- Python/MachineLearning/handson-ml-master/08_dimensionality_reduction_02.py
+++ Python/MachineLearning/handson-ml-master/08_dimensionality_reduction_02.py
@@ -28,10 +28,14 @@ from sklearn.model_selection import GridSearchCV
 
 from sklearn.linear_model import LogisticRegression
 
+from sklearn.neighbors import KNeighborsClassifier
+
 from sklearn.mixture import GaussianMixture
 
 from sklearn.cluster import KMeans
 from sklearn.cluster import MiniBatchKMeans
+from sklearn.cluster import DBSCAN
+
 
 from sklearn.metrics import silhouette_score
 from sklearn.metrics import silhouette_samples
@@ -331,3 +335,148 @@ print   ('----------------------------------------------------------------------
         '          DBSCAN                                                                                       \n'
         '------------------------------------------------------------------------------------------------------\n'
         )
+# prepare used data by make_moons
+X, y = make_moons(n_samples=1000, noise=0.05, random_state=42)
+
+# confirm the data by make_moons
+plt.figure(figsize=(8, 6))
+plt.title("a raw data by make_moons")
+plt.scatter(X[:, 0][y ==0], X[:, 1][y == 0], c='red', label="y = 0")
+plt.scatter(X[:, 0][y ==1], X[:, 1][y == 1], c='blue', label="y = 1")
+plt.grid(True)
+plt.xlabel("X0")
+plt.ylabel("X1")
+plt.legend()
+plt.show()
+
+# used clustering is DBSCAN
+dbscan = DBSCAN(eps=0.05, min_samples=5)
+
+dbscan_fit = dbscan.fit(X)
+print('dbscan_fit = \n{0}\n'.format(dbscan_fit))
+
+print('dbscan.labels_[:10] = \n{0}\n'.format(dbscan.labels_[:10]))
+
+print('dbscan.components_[:3] = \n{0}\n'.format(dbscan.components_[:3]))
+
+dbscan_unique_label = np.unique(dbscan.labels_)
+print('dbscan_unique_label = \n{0}\n'.format(dbscan_unique_label))
+
+dbscan2 = DBSCAN(eps=0.2)
+
+dbscan2_fit = dbscan2.fit(X)
+print('dbscan2_fit = \n{0}\n'.format(dbscan2_fit))
+
+def plot_dbscan(dbscan, X, size, show_xlabels=True, show_ylabels=True):
+    core_mask = np.zeros_like(dbscan.labels_, dtype=bool)
+    core_mask[dbscan.core_sample_indices_] = True
+    anomalies_mask = dbscan.labels_ == -1
+    non_core_mask = ~(core_mask | anomalies_mask)
+
+    cores = dbscan.components_
+    anomalies = X[anomalies_mask]
+    non_cores = X[non_core_mask]
+    
+    plt.scatter(cores[:, 0], cores[:, 1],
+                c=dbscan.labels_[core_mask], marker='o', s=size, cmap="Paired")
+    plt.scatter(cores[:, 0], cores[:, 1], marker='*', s=20, c=dbscan.labels_[core_mask])
+    plt.scatter(anomalies[:, 0], anomalies[:, 1],
+                c="r", marker="x", s=100)
+    plt.scatter(non_cores[:, 0], non_cores[:, 1], c=dbscan.labels_[non_core_mask], marker=".")
+    if show_xlabels:
+        plt.xlabel("$x_1$", fontsize=14)
+    else:
+        plt.tick_params(labelbottom=False)
+    if show_ylabels:
+        plt.ylabel("$x_2$", fontsize=14, rotation=0)
+    else:
+        plt.tick_params(labelleft=False)
+    plt.title("eps={:.2f}, min_samples={}".format(dbscan.eps, dbscan.min_samples), fontsize=14)
+
+# to compare eps=0.05 and eps=0.20 in DBSCAN
+plt.figure(figsize=(9, 3.2))
+
+plt.subplot(121)
+plot_dbscan(dbscan, X, size=100)
+
+plt.subplot(122)
+plot_dbscan(dbscan2, X, size=600, show_ylabels=False)
+
+save_fig("dbscan_diagram")
+plt.show()
+
+dbscan = dbscan2
+
+# 
+knn = KNeighborsClassifier(n_neighbors=50)
+
+knn_fit = knn.fit(dbscan.components_, dbscan.labels_[dbscan.core_sample_indices_])
+print('knn_fit = \n{0}\n'.format(knn_fit))
+
+# create Data for verification
+X_new = np.array([[-0.5, 0], [0, 0.5], [1, -0.1], [2, 1]])
+
+# confirm prediction data
+knn_predict = knn.predict(X_new)
+print('knn_predict = \n{0}\n'.format(knn_predict))
+
+# Calculate probability estimates for X_new.
+knn_predict_proba = knn.predict_proba(X_new)
+print('knn_predect_proba = \n{0}\n'.format(knn_predict_proba))
+
+def plot_data(X):
+    plt.plot(X[:, 0], X[:, 1], 'k.', markersize=2)
+
+def plot_centroids(centroids, weights=None, circle_color='w', cross_color='k'):
+    if weights is not None:
+        centroids = centroids[weights > weights.max() / 10]
+    plt.scatter(centroids[:, 0], centroids[:, 1],
+                marker='o', s=30, linewidths=8,
+                color=circle_color, zorder=10, alpha=0.9)
+    plt.scatter(centroids[:, 0], centroids[:, 1],
+                marker='x', s=50, linewidths=50,
+                color=cross_color, zorder=11, alpha=1)
+
+def plot_decision_boundaries(clusterer, X, resolution=1000, show_centroids=True, show_xlabels=True, show_ylabels=True):
+    mins = X.min(axis=0) - 0.1
+    maxs = X.max(axis=0) + 0.1
+    xx, yy = np.meshgrid(np.linspace(mins[0], maxs[0], resolution), np.linspace(mins[1], maxs[1], resolution))
+    Z = clusterer.predict(np.c_[xx.ravel(), yy.ravel()])
+    Z = Z.reshape(xx.shape)
+
+    plt.contourf(Z, extent=(mins[0], maxs[0], mins[1], maxs[1]),
+                cmap="Pastel2")
+    plt.contour(Z, extent=(mins[0], maxs[0], mins[1], maxs[1]),
+                linewidths=1, colors='k')
+    plot_data(X)
+    if show_centroids:
+        plot_centroids(clusterer.cluster_centers_)
+
+    if show_xlabels:
+        plt.xlabel("$x_1$", fontsize=14)
+    else:
+        plt.tick_params(labelbottom=False)
+    if show_ylabels:
+        plt.ylabel("$x_2$", fontsize=14, rotation=0)
+    else:
+        plt.tick_params(labelleft=False)
+
+# plot knn result
+plt.figure(figsize=(6, 3))
+plot_decision_boundaries(knn, X, show_centroids=False)
+plt.scatter(X_new[:, 0], X_new[:, 1], c="b", marker="+", s=200, zorder=10)
+save_fig("cluster_classification_diagram")
+plt.show()
+
+'''
+----------------------------------------------------------------------------------------
+Finds the K-neighbors of a point. 
+Returns indices of and distances to the neighbors of each point.
+----------------------------------------------------------------------------------------
+'''
+y_dist, y_pred_idx = knn.kneighbors(X_new, n_neighbors=1)
+
+y_pred = dbscan.labels_[dbscan.core_sample_indices_][y_pred_idx]
+y_pred[y_dist > 0.2] = -1
+
+print('y_pred.ravel() = \n{0}\n'.format(y_pred.ravel()))
