3a163fac692322b337c268bc837fc9401c057c28
munezou <kazumikm0119@pi5.fiberbit.net>
2019-12-12 01:12:34 +0900

    tf_function.pyを登録する。

--- Python/b_tensorflow/function/tf_function.py
+++ Python/b_tensorflow/function/tf_function.py
@@ -139,3 +139,389 @@ For example, if statements will be converted into tf.cond() if they depend on a 
 ---------------------------------------------------------------------------------------------------------------
 '''
 # In the example below, x is a Tensor but the if statement works as expected:
+@tf.function
+def square_if_positive(x):
+    if x > 0:
+        x = x * x
+    else:
+        x = 0
+    return x
+
+print('square_if_positive(2) = {0}'.format(square_if_positive(tf.constant(2))))
+print('square_if_positive(-2) = {0}\n'.format(square_if_positive(tf.constant(-2))))
+
+'''
+--------------------------------------------------------------------------------------------------------------
+Note: 
+The previous example uses simple conditionals with scalar values. 
+Batching is typically used in real-world code.
+--------------------------------------------------------------------------------------------------------------
+'''
+
+'''
+--------------------------------------------------------------------------------------------------------------
+AutoGraph supports common Python statements like while, for, if, break, continue and return, with support for nesting. 
+That means you can use Tensor expressions in the condition of while and if statements, or iterate over a Tensor in a for loop.
+--------------------------------------------------------------------------------------------------------------
+'''
+@tf.function
+def sum_even(items):
+    s = 0
+    for c in items:
+        if c % 2 > 0:
+            continue
+        s += c
+    return s
+
+sum_even_tf = sum_even(tf.constant([10, 12, 15, 20]))
+print('sum_even_tf = \n{0}'.format(sum_even_tf))
+print('sum_even = \n{0}\n'.format(sum_even))
+
+'''
+---------------------------------------------------------------------------------------------------------------
+AutoGraph also provides a low-level API for advanced users. 
+---------------------------------------------------------------------------------------------------------------
+'''
+
+# For example we can use it to have a look at the generated code.
+print(tf.autograph.to_code(sum_even.python_function))
+
+# Here's an example of more complicated control flow:
+@tf.function
+def fizzbuzz(n):
+    for i in tf.range(n):
+        if i % 3 == 0:
+            tf.print('Fizz')
+        elif i % 5 == 0:
+            tf.print('Buzz')
+        else:
+            tf.print(i)
+
+fizzbuzz_tf_const = fizzbuzz(tf.constant(15))
+print('fizzbuzz_tf_const = \n{0}\n'.format(fizzbuzz_tf_const))
+
+print   (
+        '------------------------------------------------------------------------------------------------------\n'
+        '       Keras and AutoGraph                                                                            \n'
+        '------------------------------------------------------------------------------------------------------\n'
+        )
+'''
+---------------------------------------------------------------------------------------------------------------
+AutoGraph is available by default in non-dynamic Keras models. For more information, see tf.keras.
+---------------------------------------------------------------------------------------------------------------
+'''
+
+class CustomModel(tf.keras.models.Model):
+    
+    @tf.function
+    def call(self, input_data):
+        if tf.math.reduce_mean(input_data) > 0:
+            return input_data
+        else:
+            return input_data // 2
+
+# create instance
+model = CustomModel()
+
+model_tf_const = model(tf.constant([-2, -4]))
+print('model_tf_const = {0}\n'.format(model_tf_const))
+print('model = \n{0}\n'.format(model))
+
+print   (
+        '------------------------------------------------------------------------------------------------------\n'
+        '       Side effects                                                                                   \n'
+        '------------------------------------------------------------------------------------------------------\n'
+        )
+'''
+---------------------------------------------------------------------------------------------------------------
+Just like in eager mode, 
+you can use operations with side effects, like tf.assign or tf.print normally inside tf.function, 
+and it will insert the necessary control dependencies to ensure they execute in order.
+--------------------------------------------------------------------------------------------------------------
+'''
+v = tf.Variable(5)
+
+@tf.function
+def find_next_odd():
+    v.assign(v + 1)
+    if v % 2 == 0:
+        v.assign(v + 1)
+
+
+find_next_odd()
+
+print('v = {0}\n'.format(v))
+print(tf.autograph.to_code(find_next_odd.python_function))
+
+print   (
+        '------------------------------------------------------------------------------------------------------\n'
+        '       Debugging                                                                                      \n'
+        '------------------------------------------------------------------------------------------------------\n'
+        )
+'''
+------------------------------------------------------------------------------------------------------------------
+tf.function and AutoGraph work by generating code and tracing it into TensorFlow graphs. 
+This mechanism does not yet support step-by-step debuggers like pdb. 
+However, you can call tf.config.run_functions_eagerly(True) 
+to temporarily enable eager execution inside the `tf.function' and use your favorite debugger:
+------------------------------------------------------------------------------------------------------------------
+'''
+@tf.function
+def f(x):
+    if x > 0:
+        # Try setting a breakpoint here!
+        # Example:
+        #   import pdb
+        #   pdb.set_trace()
+        x = x + 1
+    return x
+
+tf.config.experimental_run_functions_eagerly(True)
+
+# You can now set breakpoints and run the code in a debugger.
+f_tf_const = f(tf.constant(1))
+print('f_tf_const = {0}\n'.format(f_tf_const))
+
+tf.config.experimental_run_functions_eagerly(False)
+
+print   (
+        '------------------------------------------------------------------------------------------------------\n'
+        '       Advanced example: An in-graph training loop                                                    \n'
+        '------------------------------------------------------------------------------------------------------\n'
+        )
+'''
+---------------------------------------------------------------------------------------------------------------
+The previous section showed that AutoGraph can be used inside Keras layers and models. 
+Keras models can also be used in AutoGraph code.
+
+This example shows how to train a simple Keras model on MNIST with the entire training process?loading batches, 
+calculating gradients, updating parameters, calculating validation accuracy, 
+and repeating until convergence?is performed in-graph.
+---------------------------------------------------------------------------------------------------------------
+'''
+print   (
+        '------------------------------------------------------------------------------------------------------\n'
+        '       Download data                                                                                  \n'
+        '------------------------------------------------------------------------------------------------------\n'
+        )
+def prepare_mnist_features_and_labels(x, y):
+    x = tf.cast(x, tf.float32) / 255.0
+    y = tf.cast(y, tf.int64)
+    return x, y
+
+def mnist_dataset():
+    (x, y), _ = tf.keras.datasets.mnist.load_data()
+    ds = tf.data.Dataset.from_tensor_slices((x, y))
+    ds = ds.map(prepare_mnist_features_and_labels)
+    ds = ds.take(20000).shuffle(20000).batch(100)
+    return ds
+
+train_dataset = mnist_dataset()
+
+print   (
+        '------------------------------------------------------------------------------------------------------\n'
+        '       Define the model                                                                               \n'
+        '------------------------------------------------------------------------------------------------------\n'
+        )
+
+model = tf.keras.Sequential ((
+                            tf.keras.layers.Reshape(target_shape=(28 * 28,), input_shape=(28, 28)),
+                            tf.keras.layers.Dense(100, activation='relu'),
+                            tf.keras.layers.Dense(100, activation='relu'),
+                            tf.keras.layers.Dense(10)
+                            ))
+
+model.build()
+
+optimizer = tf.keras.optimizers.Adam()
+
+print   (
+        '------------------------------------------------------------------------------------------------------\n'
+        '       Define the training loop                                                                       \n'
+        '------------------------------------------------------------------------------------------------------\n'
+        )
+
+compute_loss = tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True)
+
+compute_accuracy = tf.keras.metrics.SparseCategoricalAccuracy()
+
+
+def train_one_step(model, optimizer, x, y):
+    with tf.GradientTape() as tape:
+        logits = model(x)
+        loss = compute_loss(y, logits)
+
+    grads = tape.gradient(loss, model.trainable_variables)
+    optimizer.apply_gradients(zip(grads, model.trainable_variables))
+
+    compute_accuracy(y, logits)
+    return loss
+
+
+@tf.function
+def train(model, optimizer):
+    train_ds = mnist_dataset()
+    step = 0
+    loss = 0.0
+    accuracy = 0.0
+    for x, y in train_ds:
+        step += 1
+        loss = train_one_step(model, optimizer, x, y)
+        if step % 10 == 0:
+            tf.print('Step', step, ': loss', loss, '; accuracy', compute_accuracy.result())
+    return step, loss, accuracy
+
+step, loss, accuracy = train(model, optimizer)
+print('Final step', step, ': loss', loss, '; accuracy', compute_accuracy.result())
+
+print   (
+        '------------------------------------------------------------------------------------------------------\n'
+        '       Batching                                                                                       \n'
+        '------------------------------------------------------------------------------------------------------\n'
+        )
+'''
+---------------------------------------------------------------------------------------------------------------
+In real applications batching is essential for performance. 
+The best code to convert to AutoGraph is code where the control flow is decided at the batch level. 
+If making decisions at the individual example level, try to use batch APIs to maintain performance.
+---------------------------------------------------------------------------------------------------------------
+'''
+# For example, if you have the following code in Python:
+def square_if_positive(x):
+    return [i ** 2 if i > 0 else i for i in x]
+
+square_if_positive_result = square_if_positive(range(-5, 5))
+print('square_if_positive_result = \n{0}\n'.format(square_if_positive_result))
+
+# You may be tempted to write it in TensorFlow as such (and this would work!):
+@tf.function
+def square_if_positive_naive(x):
+    result = tf.TensorArray(tf.int32, size=x.shape[0])
+    for i in tf.range(x.shape[0]):
+        if x[i] > 0:
+            result = result.write(i, x[i] ** 2)
+        else:
+            result = result.write(i, x[i])
+    return result.stack()
+
+square_if_positive_native_result = square_if_positive_naive(tf.range(-5, 5))
+print('square_if_positive_native_result = \n{0}\n'.format(square_if_positive_native_result))
+
+# But in this case, it turns out you can write the following:
+def square_if_positive_vectorized(x):
+    return tf.where(x > 0, x ** 2, x)
+
+square_if_positive_vectorized_result = square_if_positive_vectorized(tf.range(-5, 5))
+print('square_if_positive_vectorized_result = \n{0}\n'.format(square_if_positive_vectorized_result))
+
+print   (
+        '------------------------------------------------------------------------------------------------------\n'
+        '       Re-tracing                                                                                     \n'
+        '------------------------------------------------------------------------------------------------------\n'
+        )
+'''
+---------------------------------------------------------------------------------------------------------------
+Key points:
+
+Exercise caution when calling functions with non-tensor arguments, or with arguments that change shapes.
+Decorate module-level functions, and methods of module-level classes, and avoid decorating local functions or methods.
+tf.function can give you significant speedup over eager execution, at the cost of a slower first-time execution. 
+This is because when executed for the first time, the function is also traced into a TensorFlow graph. 
+Constructing and optimizing a graph is usually much slower compared to actually executing it:
+----------------------------------------------------------------------------------------------------------------
+'''
+@tf.function
+def f(x, y):
+    return tf.matmul(x, y)
+
+print(
+    "First invocation:",
+    timeit.timeit(lambda: f(tf.ones((10, 10)), tf.ones((10, 10))), number=1))
+
+print(
+    "Second invocation:",
+    timeit.timeit(lambda: f(tf.ones((10, 10)), tf.ones((10, 10))), number=1))
+
+'''
+--------------------------------------------------------------------------------------------------------------------
+You can easily tell when a function is traced by adding a print statement to the top of the function. 
+Because any Python code is only executed at trace time, you will only see the otput of print when the function is traced:
+--------------------------------------------------------------------------------------------------------------------
+'''
+@tf.function
+def f():
+    print('Tracing!')
+    tf.print('Executing')
+
+print('First invocation:')
+f()
+
+print('Second invocation:')
+f()
+
+print   (
+        '------------------------------------------------------------------------------------------------------\n'
+        '       tf.function may also re-trace when called with different non-tensor arguments                  \n'
+        '------------------------------------------------------------------------------------------------------\n'
+        )
+@tf.function
+def f(n):
+    print(n, 'Tracing!')
+    tf.print(n, 'Executing')
+
+f(1)
+f(1)
+
+print()
+
+f(2)
+f(2)
+print()
+
+print   (
+        '------------------------------------------------------------------------------------------------------\n'
+        '        A re-trace can also happen when tensor arguments change shape,                                \n'
+        '        unless you specified an input_signature:                                                      \n'
+        '------------------------------------------------------------------------------------------------------\n'
+        )
+@tf.function
+def f(x):
+    print(x.shape, 'Tracing!')
+    tf.print(x, 'Executing')
+
+f(tf.constant([1]))
+f(tf.constant([2]))
+
+print()
+
+f(tf.constant([1, 2]))
+f(tf.constant([3, 4]))
+
+print   (
+        '------------------------------------------------------------------------------------------------------\n'
+        '    In addition,                                                                                      \n'
+        '    tf.function always creates a new graph function with its own set of traces whenever it is called: \n'
+        '------------------------------------------------------------------------------------------------------\n'
+        )
+def f():
+    print('Tracing!')
+    tf.print('Executing')
+
+tf.function(f)()
+tf.function(f)()
+
+print   (
+        '------------------------------------------------------------------------------------------------------\n'
+        '    This can lead to surprising behavior                                                              \n'
+        '    when using the @tf.function decorator in a nested function:                                       \n'
+        '------------------------------------------------------------------------------------------------------\n'
+        )
+def outer():
+    @tf.function
+    def f():
+        print('Tracing!')
+        tf.print('Executing')
+    f()
+
+outer()
+outer()
