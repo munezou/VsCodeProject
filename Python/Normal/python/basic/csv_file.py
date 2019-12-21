588ff480792a66f171f5cf0f036220f7ed4de86a
munezou <kazumikm0119@pi5.fiberbit.net>
2019-12-21 13:30:48 +0900

    csv_file.pyを登録する。

--- Python/Normal/python/basic/csv_file.py
+++ Python/Normal/python/basic/csv_file.py
@@ -38,11 +38,66 @@ print   (
 
 pd.options.display.max_columns = None
 
-print("TensorFlow version: ", tf.__version__)
-assert version.parse(tf.__version__).release[0] >= 2, \
-"This notebook requires TensorFlow 2.0 or above."
-
 pf = platform.system()
 PROJECT_ROOT_DIR = os.path.join(os.path.abspath(os.path.dirname(__file__)))
 
-file_path = os.path.join(PROJECT_ROOT_DIR, "csv_data", "")
+dir_path = os.path.join(PROJECT_ROOT_DIR, "csv_data", "")
+
+# how to write csv file
+file_name = "test.csv"
+file_path = os.path.join(dir_path, file_name)
+
+# prepare writting data
+list_array = [1, 4, 5, 9, 99, 54]
+list_array2D = [
+        [1, 2],
+        [3, 4],
+        [5, 6]
+    ]
+
+# Write CSV file using with
+with open(file_path, 'w') as f:
+    writer = csv.writer(f, lineterminator='\n')
+    writer.writerow(list_array)
+    writer.writerows(list_array2D)
+
+print('---< Read CSV file using with >---') 
+with open(file_path, 'r') as f:
+    reader = csv.reader(f)
+    header = next(reader)
+
+    for row in reader:
+        print(row)
+
+file_name = "test_m.csv"
+file_path = os.path.join(dir_path, file_name)
+
+# prepare writting data
+list_array = [54, 99, 9, 5, 4, 1]
+list_array2D = [
+        [6, 5],
+        [4, 3],
+        [2, 1]
+    ]
+
+print()
+print()
+
+# Write CSV file using without
+f = open(file_path, 'w')
+
+writer = csv.writer(f, lineterminator='\n')
+writer.writerow(list_array)
+writer.writerows(list_array2D)
+
+f.close()
+
+print('---< Read CSV file using without >---') 
+f = open(file_path, 'r')
+
+reader = csv.reader(f)
+header = next(reader)
+for row in reader:
+    print(row)
+
+f.close()
