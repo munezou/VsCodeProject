b6ba0e12bd7c09158acd891eaea0716ba1ee90a7
munezou <kazumikm0119@pi5.fiberbit.net>
2019-12-30 03:14:14 +0900

    datasets.pyに、source codeを追加する。

--- Python/Normal/python/basic/datasets.py
+++ Python/Normal/python/basic/datasets.py
@@ -12,6 +12,7 @@ import os
 import platform
 import shutil
 import subprocess
+import random
 from pathlib import Path
 from packaging import version
 from PIL import Image
@@ -26,6 +27,8 @@ import matplotlib.pyplot as plt
 import tensorflow as tf
 
 print(__doc__)
+
+AUTOTUNE = tf.data.experimental.AUTOTUNE
 
 # Display current path
 basic_path = Path.cwd()
@@ -101,3 +104,73 @@ for data in d:
     print(data.numpy())
 print()
 
+print   (
+        '------------------------------------------------------------------------------------------------------\n'
+        '       Load the image using tf.data.                                                                  \n'
+        '------------------------------------------------------------------------------------------------------\n'
+        )
+data_root_orig = tf.keras.utils.get_file(
+        origin='https://storage.googleapis.com/download.tensorflow.org/example_images/flower_photos.tgz',
+        fname=PROJECT_ROOT_DIR.joinpath('original_data/flower_photos'), 
+        untar=True,
+        cache_dir=PROJECT_ROOT_DIR.joinpath('original_data')
+    )
+data_root = Path(data_root_orig)
+print(data_root)
+
+for item in data_root.iterdir():
+    print(item)
+
+all_image_paths = list(data_root.glob('*/*.jpg'))
+all_image_paths = [str(path) for path in all_image_paths]
+random.shuffle(all_image_paths)
+
+image_count = len(all_image_paths)
+print('image_count = {0}\n'.format(image_count))
+
+print('all_image_paths[:10] = \n{0}\n'.format(all_image_paths[:10]))
+
+attributions = (data_root/"LICENSE.txt").open(encoding='utf-8').readlines()[4:]
+attributions = [line.split(' CC-BY') for line in attributions]
+attributions = dict(attributions)
+
+def caption_image(image_path):
+    image_rel = Path(image_path).relative_to(data_root)
+    s = str(image_rel).split('\\')
+    output_print = "Image (CC BY 2.0) By " + s[0]
+    return output_print
+
+for n in range(3):
+    image_path = random.choice(all_image_paths)
+    im = Image.open(os.path.join(image_path))
+    im.show()
+    print(caption_image(image_path))
+    print()
+
+print   (
+        '------------------------------------------------------------------------------------------------------\n'
+        '       Determine the label for each image                                                             \n'
+        '------------------------------------------------------------------------------------------------------\n'
+        )
+# Output a list of labels.
+label_names = sorted(item.name for item in data_root.glob('*/') if item.is_dir())
+print('label_names = \n{0}\n'.format(label_names))
+
+# Assign an index to the label.
+label_to_index = dict((name, index) for index,name in enumerate(label_names))
+print('label_to_index = \n{0}\n'.format(label_to_index))
+
+# Create a list of file and label indices.
+all_image_labels = [label_to_index[Path(path).parent.name] for path in all_image_paths]
+print('First 10 labels indices: = {0}\n'.format(all_image_labels))
+
+print   (
+        '------------------------------------------------------------------------------------------------------\n'
+        '       Loading and shaping images                                                                     \n'
+        '------------------------------------------------------------------------------------------------------\n'
+        )
+img_path = all_image_paths[0]
+print('img_path = \n{0}\n'.format(image_path))
+
+img_raw = tf.io.read_file(img_path)
+print(repr(img_raw)[:100]+"...")
