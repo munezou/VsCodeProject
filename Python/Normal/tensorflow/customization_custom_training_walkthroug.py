770d772e3ca8fff2d864458d31c19ccc58101ae0
munezou <kazumikm0119@pi5.fiberbit.net>
2020-01-20 01:56:22 +0900

    customization_custom_training_walkthroug.pyに、source codeを追加する。

--- Python/Normal/tensorflow/customization_custom_training_walkthroug.py
+++ Python/Normal/tensorflow/customization_custom_training_walkthroug.py
@@ -145,3 +145,254 @@ label_name = column_names[-1]
 print("Features: {0}".format(feature_names))
 print("Label: {0}\n".format(label_name))
 
+'''
+-------------------------------------------------------------------------------------------------------------
+Each label is associated with string name (for example, "setosa"), 
+but machine learning typically relies on numeric values. 
+The label numbers are mapped to a named representation, such as:
+
+0: Iris setosa
+1: Iris versicolor
+2: Iris virginica
+-------------------------------------------------------------------------------------------------------------
+'''
+# For more information about features and labels, see the ML Terminology section of the Machine Learning Crash Course.
+class_names = ['Iris setosa', 'Iris versicolor', 'Iris virginica']
+
+print   (
+        '------------------------------------------------------------------------------------------------------\n'
+        '       Create a tf.data.Dataset                                                                       \n'
+        '------------------------------------------------------------------------------------------------------\n'
+        )
+'''
+---------------------------------------------------------------------------------------------------------------
+TensorFlow's Dataset API handles many common cases for loading data into a model. 
+This is a high-level API for reading data and transforming it into a form used for training. 
+See the Datasets Quick Start guide for more information.
+
+Since the dataset is a CSV-formatted text file, 
+use the make_csv_dataset function to parse the data into a suitable format. 
+Since this function generates data for training models, the default behavior is to shuffle the data (shuffle=True, shuffle_buffer_size=10000), 
+and repeat the dataset forever (num_epochs=None). We also set the batch_size parameter:
+---------------------------------------------------------------------------------------------------------------
+'''
+batch_size = 32
+
+train_dataset = tf.data.experimental.make_csv_dataset(
+                        train_dataset_fp,
+                        batch_size,
+                        column_names=column_names,
+                        label_name=label_name,
+                        num_epochs=1
+                    )
+
+'''
+----------------------------------------------------------------------------------------------------------------
+The make_csv_dataset function returns a tf.data.Dataset of (features, label) pairs, 
+where features is a dictionary: {'feature_name': value}
+
+These Dataset objects are iterable. Let's look at a batch of features:
+----------------------------------------------------------------------------------------------------------------
+'''
+
+features, labels = next(iter(train_dataset))
+
+print(features)
+
+'''
+----------------------------------------------------------------------------------------------------------------
+Notice that like-features are grouped together, 
+or batched. Each example row's fields are appended to the corresponding feature array. 
+Change the batch_size to set the number of examples stored in these feature arrays.
+----------------------------------------------------------------------------------------------------------------
+'''
+# You can start to see some clusters by plotting a few features from the batch:
+plt.scatter(
+                features['petal_length'],
+                features['sepal_length'],
+                c=labels,
+                cmap='viridis'
+            )
+
+plt.xlabel("Petal length")
+plt.ylabel("Sepal length")
+plt.show()
+
+'''
+-----------------------------------------------------------------------------------------------------------------
+To simplify the model building step, 
+create a function to repackage the features dictionary into a single array with shape: (batch_size, num_features).
+
+This function uses the tf.stack method which takes values from a list of tensors and creates a combined tensor 
+at the specified dimension:
+-----------------------------------------------------------------------------------------------------------------
+'''
+def pack_features_vector(features, labels):
+    """Pack the features into a single array."""
+    features = tf.stack(list(features.values()), axis=1)
+    return features, labels
+
+'''
+----------------------------------------------------------------------------------------------------------------
+Then use the tf.data.Dataset.map method to pack the features of each (features,label) pair into the training dataset:
+----------------------------------------------------------------------------------------------------------------
+'''
+train_dataset = train_dataset.map(pack_features_vector)
+
+'''
+----------------------------------------------------------------------------------------------------------------
+The features element of the Dataset are now arrays with shape (batch_size, num_features). 
+Let's look at the first few examples:
+----------------------------------------------------------------------------------------------------------------
+'''
+features, labels = next(iter(train_dataset))
+
+print(features[:5])
+
+print   (
+        '------------------------------------------------------------------------------------------------------\n'
+        '       Create a model using Keras                                                                     \n'
+        '------------------------------------------------------------------------------------------------------\n'
+        )
+'''
+---------------------------------------------------------------------------------------------------------------
+The TensorFlow tf.keras API is the preferred way to create models and layers. 
+This makes it easy to build models and experiment while Keras handles the complexity of connecting everything together.
+
+The tf.keras.Sequential model is a linear stack of layers. 
+Its constructor takes a list of layer instances, in this case, two Dense layers with 10 nodes each, 
+and an output layer with 3 nodes representing our label predictions. 
+The first layer's input_shape parameter corresponds to the number of features from the dataset, and is required:
+--------------------------------------------------------------------------------------------------------------
+'''
+
+model = tf.keras.Sequential([
+            tf.keras.layers.Dense(10, activation=tf.nn.relu, input_shape=(4,)),  # input shape required
+            tf.keras.layers.Dense(10, activation=tf.nn.relu),
+            tf.keras.layers.Dense(3)
+        ])
+
+'''
+-------------------------------------------------------------------------------------------------------------
+The activation function determines the output shape of each node in the layer. 
+These non-linearities are important—without them the model would be equivalent to a single layer. 
+There are many available activations, but ReLU is common for hidden layers.
+
+The ideal number of hidden layers and neurons depends on the problem and the dataset. 
+Like many aspects of machine learning, 
+picking the best shape of the neural network requires a mixture of knowledge and experimentation. 
+As a rule of thumb, increasing the number of hidden layers and neurons typically creates a more powerful model, 
+which requires more data to train effectively.
+-------------------------------------------------------------------------------------------------------------
+'''
+
+print   (
+        '------------------------------------------------------------------------------------------------------\n'
+        '       Using the model                                                                                \n'
+        '------------------------------------------------------------------------------------------------------\n'
+        )
+# Let's have a quick look at what this model does to a batch of features:
+predictions = model(features)
+predictions[:5]
+
+'''
+--------------------------------------------------------------------------------------------------------------
+Here, each example returns a logit for each class.
+
+To convert these logits to a probability for each class, use the softmax function:
+--------------------------------------------------------------------------------------------------------------
+'''
+
+tf.nn.softmax(predictions[:5])
+
+'''
+--------------------------------------------------------------------------------------------------------------
+Taking the tf.argmax across classes gives us the predicted class index. 
+But, the model hasn't been trained yet, so these aren't good predictions:
+--------------------------------------------------------------------------------------------------------------
+'''
+print("Prediction: {}".format(tf.argmax(predictions, axis=1)))
+print("    Labels: {}".format(labels))
+
+print   (
+        '------------------------------------------------------------------------------------------------------\n'
+        '       Train the model                                                                                \n'
+        '------------------------------------------------------------------------------------------------------\n'
+        )
+
+'''
+--------------------------------------------------------------------------------------------------------------
+Training is the stage of machine learning when the model is gradually optimized, or the model learns the dataset. 
+The goal is to learn enough about the structure of the training dataset to make predictions about unseen data. 
+If you learn too much about the training dataset, 
+then the predictions only work for the data it has seen and will not be generalizable. 
+This problem is called *overfitting*—it's like memorizing the answers instead of understanding how to solve a problem.
+
+The Iris classification problem is an example of supervised machine learning: the model is trained from examples that contain labels. 
+In unsupervised machine learning, the examples don't contain labels. Instead, the model typically finds patterns among the features.
+--------------------------------------------------------------------------------------------------------------
+'''
+
+print   (
+        '------------------------------------------------------------------------------------------------------\n'
+        '       Define the loss and gradient function                                                          \n'
+        '------------------------------------------------------------------------------------------------------\n'
+        )
+'''
+--------------------------------------------------------------------------------------------------------------
+Both training and evaluation stages need to calculate the model's loss. 
+This measures how off a model's predictions are from the desired label, in other words, how bad the model is performing. 
+We want to minimize, or optimize, this value.
+
+Our model will calculate its loss using the tf.keras.losses.SparseCategoricalCrossentropy function 
+which takes the model's class probability predictions and the desired label, 
+and returns the average loss across the examples.
+--------------------------------------------------------------------------------------------------------------
+'''
+
+loss_object = tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True)
+
+def loss(model, x, y):
+    y_ = model(x)
+
+    return loss_object(y_true=y, y_pred=y_)
+
+
+l = loss(model, features, labels)
+print("Loss test: {}".format(l))
+
+# Use the tf.GradientTape context to calculate the gradients used to optimize your model:
+def grad(model, inputs, targets):
+    with tf.GradientTape() as tape:
+        loss_value = loss(model, inputs, targets)
+    return loss_value, tape.gradient(loss_value, model.trainable_variables)
+
+print   (
+        '------------------------------------------------------------------------------------------------------\n'
+        '       Create an optimizer                                                                            \n'
+        '------------------------------------------------------------------------------------------------------\n'
+        )
+'''
+---------------------------------------------------------------------------------------------------------------
+An optimizer applies the computed gradients to the model's variables to minimize the loss function. 
+You can think of the loss function as a curved surface (see Figure 3) and we want to find its lowest point by walking around. 
+The gradients point in the direction of steepest ascent—so we'll travel the opposite way and move down the hill. 
+By iteratively calculating the loss and gradient for each batch, we'll adjust the model during training. 
+Gradually, the model will find the best combination of weights and bias to minimize loss. 
+And the lower the loss, the better the model's predictions.
+---------------------------------------------------------------------------------------------------------------
+'''
+im = Image.open(PROJECT_ROOT_DIR.joinpath('image/Figure 3. Optimization algorithms visualized over time in 3D space.jpg'))
+im.show()
+
+'''
+----------------------------------------------------------------------------------------------------------------
+TensorFlow has many optimization algorithms available for training. 
+This model uses the tf.keras.optimizers.SGD that implements the stochastic gradient descent (SGD) algorithm. 
+The learning_rate sets the step size to take for each iteration down the hill. 
+This is a hyperparameter that you'll commonly adjust to achieve better results.
+----------------------------------------------------------------------------------------------------------------
+'''
+# Let's setup the optimizer:
+optimizer = tf.keras.optimizers.SGD(learning_rate=0.01)
+
