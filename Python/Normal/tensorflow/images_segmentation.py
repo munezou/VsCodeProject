8f25225dc9705c6573c8466c48667a615f65a800
munezou <kazumikm0119@pi5.fiberbit.net>
2020-02-04 23:06:37 +0900

    tensorflow_exampleを登録する。

---
+++ Python/Normal/tensorflow/images_segmentation.py
@@ -0,0 +1,137 @@
+'''
+------------------------------------------------------------------------------------------
+images
+    TImage segmentation
+
+This tutorial focuses on the task of image segmentation, using a modified U-Net.
+
+What is image segmentation?
+
+So far you have seen image classification, where the task of the network is to assign a label or class to an input image. 
+However, suppose you want to know where an object is located in the image, the shape of that object, which pixel belongs to which object, etc. 
+In this case you will want to segment the image, i.e., each pixel of the image is given a label. 
+Thus, the task of image segmentation is to train a neural network to output a pixel-wise mask of the image. 
+This helps in understanding the image at a much lower level, i.e., the pixel level. 
+Image segmentation has many applications in medical imaging, self-driving cars and satellite imaging to name a few.
+
+The dataset that will be used for this tutorial is the Oxford-IIIT Pet Dataset, created by Parkhi et al. The dataset consists of images, 
+their corresponding labels, and pixel-wise masks. The masks are basically labels for each pixel. Each pixel is given one of three categories :
+
+	* Class 1 : Pixel belonging to the pet.
+	* Class 2 : Pixel bordering the pet.
+	* Class 3 : None of the above/ Surrounding pixel.
+------------------------------------------------------------------------------------------
+'''
+# common library
+from __future__ import absolute_import, division, print_function, unicode_literals
+
+import time
+import os
+import sys
+import pprint
+import contextlib
+from pathlib import Path
+from packaging import version
+from PIL import Image
+import tempfile
+
+import numpy as np
+import pandas as pd
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+
+
+import tensorflow as tf
+import tensorflow_hub as hub
+import tensorflow_datasets as tfds
+
+from tensorflow.keras import layers
+print(__doc__)
+
+sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
+
+from tensorflow_examples.models.pix2pix import pix2pix
+
+keras = tf.keras
+tfds.disable_progress_bar()
+
+AUTOTUNE = tf.data.experimental.AUTOTUNE
+
+pd.options.display.max_rows = None
+
+# Display current path
+basic_path = Path.cwd()
+PROJECT_ROOT_DIR = basic_path.joinpath('Python/Normal/tensorflow')
+print('PROJECT_ROOT_DIR = \n{0}\n'.format(PROJECT_ROOT_DIR))
+
+# Display tensorflow version
+print("TensorFlow version: ", tf.version.VERSION)
+assert version.parse(tf.version.VERSION).release[0] >= 2, \
+"This notebook requires TensorFlow 2.0 or above."
+
+pprint.pprint(sys.path)
+
+print   (
+        '------------------------------------------------------------------------------------------------------\n'
+        '       Download the Oxford-IIIT Pets dataset                                                          \n'
+        '------------------------------------------------------------------------------------------------------\n'
+        )
+'''
+---------------------------------------------------------------------------------------------------------------
+The dataset is already included in TensorFlow datasets, all that is needed to do is download it. 
+The segmentation masks are included in version 3.0.0, which is why this particular version is used.
+---------------------------------------------------------------------------------------------------------------
+'''
+dataset, info = tfds.load(
+                    'oxford_iiit_pet:3.0.0', 
+                    data_dir=PROJECT_ROOT_DIR.joinpath('Data'),
+                    with_info=True,
+                    as_supervised=True
+                )
+
+print('info = \n{0}\n'.format(info))
+
+'''
+----------------------------------------------------------------------------------------------------------------
+The following code performs a simple augmentation of flipping an image. 
+In addition, image is normalized to [0,1]. 
+Finally, as mentioned above the pixels in the segmentation mask are labeled either {1, 2, 3}. 
+For the sake of convinience, let's subtract 1 from the segmentation mask, resulting in labels that are : {0, 1, 2}.
+----------------------------------------------------------------------------------------------------------------
+'''
+
+def normalize(input_image, input_mask):
+    input_image = tf.cast(input_image, tf.float32) / 255.0
+    input_mask -= 1
+    return input_image, input_mask
+
+@tf.function
+def load_image_train(datapoint):
+    input_image = tf.image.resize(datapoint['image'], (128, 128))
+    input_mask = tf.image.resize(datapoint['segmentation_mask'], (128, 128))
+
+    if tf.random.uniform(()) > 0.5:
+        input_image = tf.image.flip_left_right(input_image)
+        input_mask = tf.image.flip_left_right(input_mask)
+
+    input_image, input_mask = normalize(input_image, input_mask)
+
+    return input_image, input_mask
+
+def load_image_test(datapoint):
+    input_image = tf.image.resize(datapoint['image'], (128, 128))
+    input_mask = tf.image.resize(datapoint['segmentation_mask'], (128, 128))
+
+    input_image, input_mask = normalize(input_image, input_mask)
+
+    return input_image, input_mask
+
+'''
+------------------------------------------------------------------------------------------------------------
+The dataset already contains the required splits of test and train and so let's continue to use the same split.
+------------------------------------------------------------------------------------------------------------
+'''
+TRAIN_LENGTH = info.splits['train'].num_examples
+BATCH_SIZE = 64
+BUFFER_SIZE = 1000
+STEPS_PER_EPOCH = TRAIN_LENGTH // BATCH_SIZE
