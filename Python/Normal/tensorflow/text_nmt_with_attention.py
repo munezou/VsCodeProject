abc93cad0323263ff26a90574c7e491506f6a83d
munezou <kazumikm0119@pi5.fiberbit.net>
2020-02-22 22:15:20 +0900

    text_nmt_with_attention.pyを登録する。

--- Python/Normal/tensorflow/text_nmt_with_attention.py
+++ Python/Normal/tensorflow/text_nmt_with_attention.py
@@ -68,3 +68,71 @@ print   (
         '      Download and prepare the dataset                                                                \n'
         '------------------------------------------------------------------------------------------------------\n'
         )
+'''
+--------------------------------------------------------------------------------------------------------------
+We'll use a language dataset provided by http://www.manythings.org/anki/. 
+This dataset contains language translation pairs in the format:
+
+May I borrow this book?    ¿Puedo tomar prestado este libro?
+There are a variety of languages available, but we'll use the English-Spanish dataset. 
+For convenience, we've hosted a copy of this dataset on Google Cloud, but you can also download your own copy. 
+After downloading the dataset, here are the steps we'll take to prepare the data:
+
+	1. Add a start and end token to each sentence.
+	2. Clean the sentences by removing special characters.
+	3. Create a word index and reverse word index (dictionaries mapping from word → id and id → word).
+	4. Pad each sentence to a maximum length.
+--------------------------------------------------------------------------------------------------------------
+'''
+spa_eng_path = str(PROJECT_ROOT_DIR.joinpath('Data/spa_eng/spa-eng.zip'))
+
+# Download the file
+path_to_zip = tf.keras.utils.get_file(
+                fname=spa_eng_path, 
+                origin='http://storage.googleapis.com/download.tensorflow.org/data/spa-eng.zip',
+                extract=True,
+                cache_dir=PROJECT_ROOT_DIR.joinpath('Data/spa_eng')
+            )
+
+path_to_file = str(PROJECT_ROOT_DIR.joinpath('Data/spa_eng/datasets/spa-eng/spa.txt'))
+
+# Converts the unicode file to ascii
+def unicode_to_ascii(s):
+    return ''.join(c for c in unicodedata.normalize('NFD', s)
+    if unicodedata.category(c) != 'Mn')
+
+
+def preprocess_sentence(w):
+    w = unicode_to_ascii(w.lower().strip())
+
+    # creating a space between a word and the punctuation following it
+    # eg: "he is a boy." => "he is a boy ."
+    # Reference:- https://stackoverflow.com/questions/3645931/python-padding-punctuation-with-white-spaces-keeping-punctuation
+    w = re.sub(r"([?.!,¿])", r" \1 ", w)
+    w = re.sub(r'[" "]+', " ", w)
+
+    # replacing everything with space except (a-z, A-Z, ".", "?", "!", ",")
+    w = re.sub(r"[^a-zA-Z?.!,¿]+", " ", w)
+
+    w = w.rstrip().strip()
+
+    # adding a start and an end token to the sentence
+    # so that the model know when to start and stop predicting.
+    w = '<start> ' + w + ' <end>'
+    return w
+
+en_sentence = u"May I borrow this book?"
+sp_sentence = u"¿Puedo tomar prestado este libro?"
+print(preprocess_sentence(en_sentence))
+print(preprocess_sentence(sp_sentence).encode('utf-8'))
+
+# 1. Remove the accents
+# 2. Clean the sentences
+# 3. Return word pairs in the format: [ENGLISH, SPANISH]
+def create_dataset(path, num_examples):
+    lines = io.open(path, encoding='UTF-8').read().strip().split('\n')
+
+    word_pairs = [[preprocess_sentence(w) for w in l.split('\t')]  for l in lines[:num_examples]]
+
+    return zip(*word_pairs)
+
