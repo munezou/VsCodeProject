4070303d8d97fbbb10138ec937254b7897274dae
munezou <kazumikm0119@pi5.fiberbit.net>
2020-03-25 22:32:49 +0900

    tensors.pyを修正する。

--- Python/b_tensorflow/tensorflow_cookbook_master/cp01_Introduction/sec02_Creating_and_Using_Tensors/tensors.py
+++ Python/b_tensorflow/tensorflow_cookbook_master/cp01_Introduction/sec02_Creating_and_Using_Tensors/tensors.py
@@ -17,6 +17,7 @@ print(__doc__)
 import os
 import numpy as np
 import math
+from datetime import datetime
 
 import tensorflow as tf
 
@@ -28,7 +29,6 @@ print('PROJECT_ROOT_DIR = \n{0}\n'.format(PROJECT_ROOT_DIR))
 print("TensorFlow version: ", tf.version.VERSION)
 
 # Introduce tensors in tf
-
 my_tensor = tf.zeros([1,20])
 print('my_tensor = \n{0}\n'.format(my_tensor))
 
@@ -66,19 +66,41 @@ print('const_fill_tensor = \n{0}\n'.format(const_fill_tensor))
 linear_tensor = tf.linspace(start=0.0, stop=1.0, num=3) # Generates [0.0, 0.5, 1.0] includes the end
 print('linear_tensor = \n{0}\n'.format(linear_tensor))
 
-sequence_tensor = tf.range(start=6, limit=15, delta=3) # Generates [6, 9, 12] doesn't include the end
-print('sequence_tensor = \n{0}\n'.format(sequence_tensor))
+# The function to be traced.
+@tf.function
+def my_func(x, y):
+    # A simple hand-rolled layer.
+    return tf.nn.relu(tf.matmul(x, y))
 
-log_path = os.path.join(PROJECT_ROOT_DIR, 'logs\\tmp')
+# Set up logging.
+stamp = datetime.now().strftime("%Y%m%d-%H%M%S")
+logdir = os.path.join(PROJECT_ROOT_DIR, "logs", "func", datetime.now().strftime("%Y%m%d-%H%M%S"))
+writer = tf.summary.create_file_writer(logdir)
+
+# Sample data for your function.
+x = tf.random.uniform((3, 3))
+y = tf.random.uniform((3, 3))
+
+# Bracket the function call with
+# tf.summary.trace_on() and tf.summary.trace_export().
+tf.summary.trace_on(graph=True, profiler=True)
+# Call only one tf.function when tracing.
+z = my_func(x, y)
+linear_tensor = tf.linspace(start=0.0, stop=1.0, num=3)
+z = z + linear_tensor
+with writer.as_default():
+    tf.summary.trace_export(
+        name="my_func_trace",
+        step=3,
+        profiler_outdir=logdir)
 
 
+'''
 # Random Numbers
 
 # Random Normal
 rnorm_tensor = tf.random_normal([row_dim, col_dim], mean=0.0, stddev=1.0)
 
-
-'''
 # Add summaries to tensorboard
 merged = tf.summary.merge_all()
 
