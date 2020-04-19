2f31b72c417404a1d71830224668b4cbb848bb57
munezou <kazumikm0119@pi5.fiberbit.net>
2020-04-19 16:31:02 +0900

    data_gathering.pyのbugfix

--- Python/lect_tensorflow/tensorflow_cookbook_master/sec01_Introduction/chpt07_Working_with_Data_Sources/data_gathering.py
+++ Python/lect_tensorflow/tensorflow_cookbook_master/sec01_Introduction/chpt07_Working_with_Data_Sources/data_gathering.py
@@ -11,8 +11,13 @@
 from __future__ import absolute_import, division, print_function, unicode_literals
 import os
 import sys
+import io
 import requests
 import matplotlib.pyplot as plt
+import tarfile
+import codecs
+from PIL import Image
+from zipfile import ZipFile
 import tensorflow as tf
 from packaging import version
 
@@ -135,55 +140,97 @@ print('len(y_test) = {0}'.format(len(y_test)))
 print('y_train[5] = {0}'.format(y_train[5]))
 print('one_hot_y_train[5, :] = {0}\n'.format(one_hot_y_train[5, :]))
 
+print   (
+        '------------------------------------------------------------------------------------------------------\n'
+        '       CIFAR-10 Image Category Dataset                                                                \n'
+        '------------------------------------------------------------------------------------------------------\n'
+        )
+'''
+--------------------------------------------------------------------------------------------------------------
+The CIFAR-10 data ( https://www.cs.toronto.edu/~kriz/cifar.html ) contains 60,000 32x32 color images of 10 classes collected by Alex Krizhevsky, 
+Vinod Nair, and Geoffrey Hinton. Alex Krizhevsky maintains the page referenced here. 
+This is such a common dataset, that there are built in functions in TensorFlow to access this data (the keras wrapper has these commands). 
+Note that the keras wrapper for these functions automatically splits the images into a 50,000 training set and a 10,000 test set.
+--------------------------------------------------------------------------------------------------------------
+'''
+# Running this command requires an internet connection and a few minutes to download all the images.
+(X_train, y_train), (X_test, y_test) = tf.keras.datasets.cifar10.load_data()
 
 '''
-# CIFAR-10 Image Category Dataset
-# The CIFAR-10 data ( https://www.cs.toronto.edu/~kriz/cifar.html ) contains 60,000 32x32 color images of 10 classes.
-# It was collected by Alex Krizhevsky, Vinod Nair, and Geoffrey Hinton.
-# Alex Krizhevsky maintains the page referenced here.
-# This is such a common dataset, that there are built in functions in TensorFlow to access this data.
+-------------------------------------------------------------------------------------------------------------
+Downloading data from http://www.cs.toronto.edu/~kriz/cifar-10-python.tar.gz
+The ten categories are (in order):
 
-# Running this command requires an internet connection and a few minutes to download all the images.
-(X_train, y_train), (X_test, y_test) = tf.contrib.keras.datasets.cifar10.load_data()
-
-print(X_train.shape)
-print(y_train.shape)
-print(y_train[0,])  # this is a frog
+    0.Airplane
+    1.Automobile
+    2.Bird
+    3.Car
+    4.Deer
+    5.Dog
+    6.Frog
+    7.Horse
+    8.Ship
+    9.Truck
+-------------------------------------------------------------------------------------------------------------
+'''
+print('X_train.shape = {0}'.format(X_train.shape))
+print('y_train.shape = {0}'.format(y_train.shape))
+print('y_train[0,] = {0}'.format(y_train[0,]))
 
 # Plot the 0-th image (a frog)
-from PIL import Image
 img = Image.fromarray(X_train[0,:,:,:])
 plt.imshow(img)
+plt.show()
 
+print   (
+        '------------------------------------------------------------------------------------------------------\n'
+        '       Ham/Spam Texts Dataset (UCI)                                                                   \n'
+        '------------------------------------------------------------------------------------------------------\n'
+        )
+'''
+---------------------------------------------------------------------------------------------------------------
+We will use another UCI ML Repository dataset called the SMS Spam Collection. 
+You can read about it here: https://archive.ics.uci.edu/ml/datasets/SMS+Spam+Collection . 
+As a sidenote about common terms, when predicting if a data point represents 'spam' (or unwanted advertisement), 
+the alternative is called 'ham' (or useful information).
 
-# Ham/Spam Text Data
-import requests
-import io
-from zipfile import ZipFile
-
+This is a great dataset for predicting a binary outcome (spam/ham) from a textual input. 
+This will be very useful for short text sequences for Natural Language Processing (Ch 7) and Recurrent Neural Networks (Ch 9).
+---------------------------------------------------------------------------------------------------------------
+'''
 # Get/read zip file
 zip_url = 'http://archive.ics.uci.edu/ml/machine-learning-databases/00228/smsspamcollection.zip'
 r = requests.get(zip_url)
 z = ZipFile(io.BytesIO(r.content))
 file = z.read('SMSSpamCollection')
+
 # Format Data
 text_data = file.decode()
 text_data = text_data.encode('ascii',errors='ignore')
 text_data = text_data.decode().split('\n')
 text_data = [x.split('\t') for x in text_data if len(x)>=1]
 [text_data_target, text_data_train] = [list(x) for x in zip(*text_data)]
-print(len(text_data_train))
-print(set(text_data_target))
-print(text_data_train[1])
 
+print('len(text_data_train) = {0}'.format(len(text_data_train)))
+print('set(text_data_target) = {0}'.format(set(text_data_target)))
+print('text_data_train[1] = \n{0}\n'.format(text_data_train[1]))
 
-# Movie Review Data
-import requests
-import io
-import tarfile
+print   (
+        '------------------------------------------------------------------------------------------------------\n'
+        '       Movie Review Data                                                                              \n'
+        '------------------------------------------------------------------------------------------------------\n'
+        )
+'''
+---------------------------------------------------------------------------------------------------------------
+The Movie Review database, collected by Bo Pang and Lillian Lee (researchers at Cornell), 
+serves as a great dataset to use for predicting a numerical number from textual inputs.
 
+You can read more about the dataset and papers using it here: https://www.cs.cornell.edu/people/pabo/movie-review-data/
+---------------------------------------------------------------------------------------------------------------
+'''
 movie_data_url = 'http://www.cs.cornell.edu/people/pabo/movie-review-data/rt-polaritydata.tar.gz'
 r = requests.get(movie_data_url)
+
 # Stream data into temp object
 stream_data = io.BytesIO(r.content)
 tmp = io.BytesIO()
@@ -194,53 +241,100 @@ while True:
     tmp.write(s)
 stream_data.close()
 tmp.seek(0)
+
 # Extract tar file
 tar_file = tarfile.open(fileobj=tmp, mode="r:gz")
+
 pos = tar_file.extractfile('rt-polaritydata/rt-polarity.pos')
 neg = tar_file.extractfile('rt-polaritydata/rt-polarity.neg')
+
 # Save pos/neg reviews
 pos_data = []
 for line in pos:
     pos_data.append(line.decode('ISO-8859-1').encode('ascii',errors='ignore').decode())
+    
 neg_data = []
 for line in neg:
     neg_data.append(line.decode('ISO-8859-1').encode('ascii',errors='ignore').decode())
+    
 tar_file.close()
 
-print(len(pos_data))
-print(len(neg_data))
-print(neg_data[0])
+print('len(pos_data) = {0}'.format(len(pos_data)))
+print('len(neg_data) = {0}'.format(len(neg_data)))
+print('neg_data[0] = {0}\n'.format(neg_data[0]))
 
+print   (
+        '------------------------------------------------------------------------------------------------------\n'
+        '       The Complete Works of William Shakespeare (Gutenberg Project)                                  \n'
+        '------------------------------------------------------------------------------------------------------\n'
+        )
+'''
+---------------------------------------------------------------------------------------------------------------
+For training a TensorFlow Model to create text, we will train it on the complete works of William Shakespeare. 
+This can be accessed through the good work of the Gutenberg Project. 
+The Gutenberg Project frees many non-copyright books by making them accessible for free from the hard work of volunteers.
 
+You can read more about the Shakespeare works here: http://www.gutenberg.org/ebooks/100
+---------------------------------------------------------------------------------------------------------------
+'''
 # The Works of Shakespeare Data
-import requests
-
 shakespeare_url = 'http://www.gutenberg.org/cache/epub/100/pg100.txt'
 # Get Shakespeare text
 response = requests.get(shakespeare_url)
 shakespeare_file = response.content
+
 # Decode binary into string
 shakespeare_text = shakespeare_file.decode('utf-8')
+
 # Drop first few descriptive paragraphs.
 shakespeare_text = shakespeare_text[7675:]
-print(len(shakespeare_text))
+print('len(shakespeare_text) = {0}\n'.format(len(shakespeare_text)))
 
+print   (
+        '------------------------------------------------------------------------------------------------------\n'
+        '       English-German Sentence Translation Database (Manythings/Tatoeba)                              \n'
+        '------------------------------------------------------------------------------------------------------\n'
+        )
+'''
+---------------------------------------------------------------------------------------------------------------
+The Tatoeba Project is also run by volunteers 
+and is set to make the most bilingual sentence translations available between many different languages. 
+Manythings.org compiles the data and makes it accessible.
 
-# English-German Sentence Translation Data
-import requests
-import io
-from zipfile import ZipFile
-sentence_url = 'http://www.manythings.org/anki/deu-eng.zip'
+http://www.manythings.org/corpus/about.html#info
+
+More bilingual sentence pairs: http://www.manythings.org/bilingual/
+---------------------------------------------------------------------------------------------------------------
+'''
+
+'''
+----------------------< no work >-------------------------
+# sentence_url = 'http://www.manythings.org/anki/deu-eng.zip'
 r = requests.get(sentence_url)
 z = ZipFile(io.BytesIO(r.content))
 file = z.read('deu.txt')
+
+
 # Format Data
 eng_ger_data = file.decode()
 eng_ger_data = eng_ger_data.encode('ascii',errors='ignore')
 eng_ger_data = eng_ger_data.decode().split('\n')
 eng_ger_data = [x.split('\t') for x in eng_ger_data if len(x)>=1]
 [english_sentence, german_sentence] = [list(x) for x in zip(*eng_ger_data)]
-print(len(english_sentence))
-print(len(german_sentence))
-print(eng_ger_data[10])
+--------------------------------------------------------------
 '''
+file_path = str(os.path.join(PROJECT_ROOT_DIR, 'deu_eng\\deu.txt'))
+
+with open(file_path, encoding='utf-8') as f:
+    eng_ger_data = f.readlines()
+    english_sentence = []
+    german_sentence = []
+    for tmpStr in eng_ger_data:
+        tmp = tmpStr.split('\t')
+        english_sentence.append(tmp[0])
+        german_sentence.append(tmp[1])
+    
+
+print('len(english_sentence) = {0}'.format(len(english_sentence)))
+print('len(german_sentence) = {0}'.format(len(german_sentence)))
+print('eng_ger_data[10] = {0}\n'.format(eng_ger_data[10]))
